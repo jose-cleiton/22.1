39eeeb63c84f38bcbcfefb52043e8670500137e9
jose-cleiton <cleitons835@gmail.com>
2022-12-21 12:18:37 -0300

    feat: :sparkles: .gitignore e dependencias

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+const Operations = require('./sum');
+
 class Main {
   constructor(operations) {
-    this.operations = operations;
+    this.Operations = operations;
   }
 
   start = () => {
-    const { sum, sub, mult, div } = this.operations;
+    const { sum, sub, mult, div } = this.Operations;
     console.log(sum(2, 3));
     console.log(sub(2, 3));
     console.log(mult(2, 3));
